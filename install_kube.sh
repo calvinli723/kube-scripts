1436ee020c5dafb23fb0eb432fdf0b6c3560972b
Calvin Li <calvinli723@gmail.com>
2022-08-11 15:38:20 -0400

    install script for k8s 1.24.0-00

---
+++ install_kube.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+# create config file for containerd
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+# load modules
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# Set system config for kube networking
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+# apply new settings
+sudo sysctl --system
+
+# install containerd
+sudo apt-get update && sudo apt-get install -y containerd
+
+# create default config file for containrd
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+
+# restart containerd and verify it is running
+sudo systemctl restart containerd
+sudo systemctl status containerd
+
+# disable swap
+sudo swapoff -a
+
+# install dependencies
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+
+# download and add GPG key
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+# add k8s to repo list
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+# install k8s packages
+sudo apt-get update
+sudo apt-get install -y kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
+
+# turn off automatic updates
+sudo apt-mark hold kubelet kubeadm kubectl
