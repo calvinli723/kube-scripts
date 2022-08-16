502ef0b58793c1a72cb16707c89e128d2e18ebb9
Calvin Li <calvinli723@gmail.com>
2022-08-16 15:46:02 -0400

    init cluster and install networking scripts

---
+++ init_cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# RUN ONLY ON CONTROL PLANE NODE
+
+# initialize k8s cluster on control plane node using kubeadm
+sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.24.0
+
+# set kubectl access
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
---
+++ install_networking.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# install Calico networking
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
