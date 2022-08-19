115f1d7ecd435b7975606c29baed55d53923a2d2
Calvin Li <ctl2124@columbia.edu>
2022-08-19 18:04:10 -0400

    more reliable URL path for calico.yaml file

--- install_networking.sh
+++ install_networking.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # install Calico networking
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+#kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
