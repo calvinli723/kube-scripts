1b0b63e50d05ee064812a9223ab762942af6c5cb
Calvin Li <calvinli723@gmail.com>
2022-08-19 17:24:50 -0400

    different CIDR block for pod network

--- init_cluster.sh
+++ init_cluster.sh
@@ -2,7 +2,7 @@
 # RUN ONLY ON CONTROL PLANE NODE
 
 # initialize k8s cluster on control plane node using kubeadm
-sudo kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.24.0
+sudo kubeadm init --pod-network-cidr 10.0.0.0/16 --kubernetes-version 1.24.0
 
 # set kubectl access
 mkdir -p $HOME/.kube
