0d171271de28c9d1af0add81b5d490b72346b8f8
Calvin Li <calvinli723@gmail.com>
2023-01-30 00:07:53 -0500

    clean up install script

--- install_kube.sh
+++ install_kube.sh
@@ -23,7 +23,6 @@ sudo sysctl --system
 # install containerd
 
 sudo apt-get update
-sudo apt-get update
 sudo apt-get install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
@@ -34,10 +33,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
-sudo apt-get install containerd.io
 
-#sudo apt-get update && sudo apt-get install -y containerd.io
+sudo apt-get update && sudo apt-get install -y containerd.io
 
 # create default config file for containrd
 sudo mkdir -p /etc/containerd
