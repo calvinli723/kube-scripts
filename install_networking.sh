9f1ebf8a188dd27ae87fad32c83c1bcb420bbada
Calvin Li <calvinli723@gmail.com>
2023-01-31 16:53:05 -0500

    add metallb load balancer install

--- install_networking.sh
+++ install_networking.sh
@@ -2,5 +2,7 @@
 # RUN ONLY ON CONTROL PLANE
 
 # install Calico networking
-#kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
 kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
+
+# install MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.7/config/manifests/metallb-native.yaml
