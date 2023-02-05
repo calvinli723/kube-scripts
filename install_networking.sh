2099cf15b2f1f39d772266590fe412a6b181a15a
Calvin Tian Li <ctl2124@columbia.edu>
2023-02-05 00:35:34 -0500

    use calico release v3.25.0 instead of taking manifest directly from master branch

--- install_networking.sh
+++ install_networking.sh
@@ -3,4 +3,5 @@
 
 # install Calico networking
 #kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
-kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
+#kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/master/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml
