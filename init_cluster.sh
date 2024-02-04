ca46fbaebdebff2e43db7a19a9c1ab9917aeeb38
Calvin Li <calvinli723@gmail.com>
2024-02-04 01:23:13 -0500

    some storage stuff and lb control plane

--- init_cluster.sh
+++ init_cluster.sh
@@ -2,7 +2,8 @@
 # RUN ONLY ON CONTROL PLANE NODE
 
 # initialize k8s cluster on control plane node using kubeadm
-sudo kubeadm init --config kubeadm-config.yaml
+#sudo kubeadm init --config kubeadm-config.yaml
+sudo kubeadm init --control-plane-endpoint "939lb:6443" --upload-certs --pod-network-cidr 10.0.1.0/24
 
 # set kubectl access
 mkdir -p $HOME/.kube
